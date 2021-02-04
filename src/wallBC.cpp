ed961033f544ccbfe7d28fac25ed7047e3450c0e
marc-85 <m.bolinches@oden.utexas.edu>
2021-02-04 17:05:02 -0600

    Wall BC adapted for 3D cases

--- src/wallBC.cpp
+++ src/wallBC.cpp
@@ -47,19 +47,26 @@ void WallBC::computeINVwallFlux(Vector &normal,
                                 Vector &stateIn,
                                 Vector &bdrFlux)
 {
-  double u = stateIn[1]/stateIn[0];
-  double v = stateIn[2]/stateIn[0];
-  const double norm = sqrt(normal[0]*normal[0] + normal[1]*normal[1]);
+  Vector vel(dim);
+  for(int d=0;d<dim;d++) vel[d] = stateIn[1+d]/stateIn[0];
+  
+  double norm = 0.;
+  for(int d=0;d<dim;d++) norm += normal[d]*normal[d];
+  norm = sqrt(norm);
+  
   Vector unitN(dim);
   for(int d=0;d<dim;d++) unitN[d] = normal[d]/norm;
-  double vn = u*unitN[0] + v*unitN[1];
+  
+  double vn = 0;
+  for(int d=0;d<dim;d++) vn += vel[d]*unitN[d];
   
   Vector stateMirror(num_equation);
   
   stateMirror[0] = stateIn[0];
-  stateMirror[1] = stateIn[0]*(u -2.*vn*unitN[0]);
-  stateMirror[2] = stateIn[0]*(v -2.*vn*unitN[1]);
-  stateMirror[3] = stateIn[3];
+  stateMirror[1] = stateIn[0]*(vel[0] -2.*vn*unitN[0]);
+  stateMirror[2] = stateIn[0]*(vel[1] -2.*vn*unitN[1]);
+  if(dim==3) stateMirror[3] = stateIn[0]*(vel[2] -2.*vn*unitN[2]);
+  stateMirror[num_equation-1] = stateIn[3];
   
   rsolver->Eval(stateIn,stateMirror,normal,bdrFlux);
 }
