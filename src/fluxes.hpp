e37bdea0cc83d5d60c06e8e90151e14d2e199af8
Karl W. Schulz <karl@oden.utexas.edu>
2021-05-13 15:56:29 +0000

    performance mods: don't instantiate repeated vectors/matrices in viscous flux routine; use matvec operator

--- src/fluxes.hpp
+++ src/fluxes.hpp
@@ -24,6 +24,11 @@ private:
   const int &dim;
   
   const int &num_equations;
+  double Rg;
+  Vector gradT;
+  Vector vel;
+  Vector vtmp;
+  DenseMatrix stress;
   
 public:
   Fluxes(EquationOfState *_eqState,
