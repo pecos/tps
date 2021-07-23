36e7d50c203e36735c3fbb73ca86a174a4f37892
Karl W. Schulz <karl@oden.utexas.edu>
2021-07-23 15:21:08 -0500

    include convenience utility for writing hdf5 attributes

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,9 +1,31 @@
 #ifndef UTILS_
 #define UTILS_
 
+#include <hdf5.h>
+
 // Misc. utilities
 bool file_exists (const std::string &name);
 std::string systemCmd(const char *cmd);
+
+// HDF5 convenience utilities
+inline hid_t h5_getType( int)   { return(H5T_NATIVE_INT);    };
+inline hid_t h5_getType(double) { return(H5T_NATIVE_DOUBLE); };
+
+template <typename T> void h5_save_attribute(hid_t dest,std::string attribute, T value)
+{
+  hid_t attr, status;
+  hid_t attrType    = h5_getType(value);
+  hid_t dataspaceId = H5Screate(H5S_SCALAR);
+
+  attr = H5Acreate(dest,attribute.c_str(), attrType, dataspaceId, H5P_DEFAULT, H5P_DEFAULT);
+  assert(attr >= 0);
+  status = H5Awrite(attr,attrType,&value);
+  assert(status >= 0);
+  H5Aclose(attr);
+  H5Sclose(dataspaceId);
+}
+
+
 
 #endif
 
