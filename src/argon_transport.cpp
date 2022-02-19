2bfc1e454f1448bf7c02db924a96c5d0a2d34c0e
dreamer2368 <seung.chung@austin.utexas.edu>
2022-02-19 16:22:50 -0600

    argon ternary mixture transport: viscosity and thermal conductivity verified.

--- src/argon_transport.cpp
+++ src/argon_transport.cpp
@@ -67,7 +67,7 @@ ArgonTernaryTransport::ArgonTernaryTransport(GasMixture *_mixture, RunConfigurat
   mw_(neutralIndex_) = mixture->GetGasParams(neutralIndex_, GasParams::SPECIES_MW);
   mw_(ionIndex_) = mixture->GetGasParams(ionIndex_, GasParams::SPECIES_MW);
   // assumes input mass is consistent with this.
-  assert( mw_(ionIndex_) == mw_(neutralIndex_) - mw_(electronIndex_) );
+  assert( abs(mw_(neutralIndex_) - mw_(electronIndex_) - mw_(ionIndex_)) < 1.0e-15 );
   mw_ /= AVOGADRONUMBER;
   // mA_ /= AVOGADRONUMBER;
   // mI_ /= AVOGADRONUMBER;
@@ -91,12 +91,19 @@ void ArgonTernaryTransport::ComputeFluxTransportProperties(const Vector &state, 
   n_sp(ionIndex_) = primitiveState[dim + 2 + ionIndex_];
   n_sp(electronIndex_) = mixture->computeAmbipolarElectronNumberDensity(&primitiveState[dim + 2]);
   n_sp(neutralIndex_) = mixture->computeBackgroundMassDensity(state[0], &primitiveState[dim + 2], n_sp(electronIndex_), true);
+  n_sp(neutralIndex_) /= mixture->GetGasParams(neutralIndex_, GasParams::SPECIES_MW);
   double nTotal = n_sp(0) + n_sp(1) + n_sp(2);
   Vector X_sp(3);
   for (int sp = 0; sp < numSpecies; sp++) X_sp(sp) = n_sp(sp) / nTotal;
+  // std::cout << "N inside: ";
+  // for (int sp = 0; sp < 3; sp++) {
+  //   std::cout << n_sp(sp) << ",\t";
+  // }
+  // std::cout << std::endl;
 
   double Te = (twoTemperature) ? primitiveState[num_equation - 1] : primitiveState[dim + 1];
   double Th = primitiveState[dim + 1];
+  // std::cout << "temp: " << Th << ",\t" << Te << std::endl;
 
   double nOverT = n_sp(electronIndex_) / Te + n_sp(ionIndex_) / Th;
   double debyeLength = sqrt(debyeFactor_ / AVOGADRONUMBER / nOverT);
@@ -108,6 +115,7 @@ void ArgonTernaryTransport::ComputeFluxTransportProperties(const Vector &state, 
   Vector speciesViscosity(3);
   speciesViscosity(ionIndex_) = viscosityFactor_ * sqrt(mw_(ionIndex_) * Th) / (collision::charged::rep22(nondimTh) * debyeCircle);
   speciesViscosity(neutralIndex_) = viscosityFactor_ * sqrt(mw_(neutralIndex_) * Th) / collision::argon::ArAr22(Th);
+  // std::cout << "viscosity: " << speciesViscosity(neutralIndex_) << ",\t" << speciesViscosity(ionIndex_) << std::endl;
   speciesViscosity(electronIndex_) = 0.0;
   // speciesViscosity(0) = 5. / 16. * sqrt(PI_ * mI_ * kB_ * Th) / (collision::charged::rep22(nondimTe) * PI_ * debyeLength * debyeLength);
   for (int sp = 0; sp < numSpecies; sp++) {
@@ -128,10 +136,12 @@ void ArgonTernaryTransport::ComputeFluxTransportProperties(const Vector &state, 
 
 double ArgonTernaryTransport::computeThirdOrderElectronThermalConductivity(const Vector &X_sp, const double debyeLength, const double Te, const double nondimTe) {
   double debyeCircle = PI_ * debyeLength * debyeLength;
+  // std::cout << "LD: " << debyeLength << std::endl;
   Vector Q2(3);
   Q2(0) = debyeCircle * collision::charged::rep22(nondimTe);
   Q2(1) = debyeCircle * collision::charged::rep23(nondimTe);
   Q2(2) = debyeCircle * collision::charged::rep24(nondimTe);
+  // std::cout << "Q2r: " << Q2(0) << ",\t" << Q2(1) << ",\t" << Q2(2) << std::endl;
 
   Vector Q1Ion(5);
   Q1Ion(0) = debyeCircle * collision::charged::att11(nondimTe);
@@ -139,6 +149,9 @@ double ArgonTernaryTransport::computeThirdOrderElectronThermalConductivity(const
   Q1Ion(2) = debyeCircle * collision::charged::att13(nondimTe);
   Q1Ion(3) = debyeCircle * collision::charged::att14(nondimTe);
   Q1Ion(4) = debyeCircle * collision::charged::att15(nondimTe);
+//   std::cout << "Q1i: ";
+// for (int i = 0; i < 5; i++) std::cout << Q1Ion(i) << ",\t";
+// std::cout << std::endl;
 
   Vector Q1Neutral(5);
   Q1Neutral(0) = collision::argon::eAr11(Te);
@@ -146,6 +159,9 @@ double ArgonTernaryTransport::computeThirdOrderElectronThermalConductivity(const
   Q1Neutral(2) = collision::argon::eAr13(Te);
   Q1Neutral(3) = collision::argon::eAr14(Te);
   Q1Neutral(4) = collision::argon::eAr15(Te);
+//   std::cout << "Q1A: ";
+// for (int i = 0; i < 5; i++) std::cout << Q1Neutral(i) << ",\t";
+// std::cout << std::endl;
 
   double L11 = sqrt(2.0) * X_sp(electronIndex_) * L11ee(Q2);
   L11 += X_sp(ionIndex_) * L11ea(Q1Ion);
@@ -156,6 +172,9 @@ double ArgonTernaryTransport::computeThirdOrderElectronThermalConductivity(const
   double L22 = sqrt(2.0) * X_sp(electronIndex_) * L22ee(Q2);
   L22 += X_sp(ionIndex_) * L22ea(Q1Ion);
   L22 += X_sp(neutralIndex_) * L22ea(Q1Neutral);
+  // std::cout << "L11: " << L11 << std::endl;
+  // std::cout << "L12: " << L12 << std::endl;
+  // std::cout << "L22: " << L22 << std::endl;
 
   return viscosityFactor_ * kOverEtaFactor_ * sqrt(2.0 * Te / mw_(electronIndex_)) * X_sp(electronIndex_) / (L11 - L12 * L12 / L22);
 }
---
+++ test/test_argon_ternary.cpp
@@ -0,0 +1,186 @@
+/* A utility to transfer tps solutions from one mesh to another */
+
+#include "../src/M2ulPhyS.hpp"
+#include "mfem.hpp"
+#include <fstream>
+
+using namespace mfem;
+using namespace std;
+
+Array<int> readTable(const string fileName, const string datasetName, DenseMatrix &output) {
+  hid_t file = -1;
+  if (file_exists(fileName)) {
+    file = H5Fopen(fileName.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+  } else {
+    grvy_printf(GRVY_ERROR, "[ERROR]: Unable to open file -> %s\n", fileName.c_str());
+    exit(ERROR);
+  }
+  assert(file >= 0);
+
+  hid_t datasetID, dataspace;
+  datasetID = H5Dopen2(file, datasetName.c_str(), H5P_DEFAULT);
+  assert(datasetID >= 0);
+  dataspace = H5Dget_space(datasetID);
+  const int ndims = H5Sget_simple_extent_ndims(dataspace);
+  hsize_t dims[ndims];
+  int dummy = H5Sget_simple_extent_dims(dataspace,dims,NULL);
+
+  output.SetSize(dims[0], dims[1]);
+
+  herr_t status;
+  status = H5Dread(datasetID, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, output.HostReadWrite());
+  assert(status >= 0);
+  H5Dclose(datasetID);
+  H5Fclose(file);
+
+  Array<int> shape(2);
+  shape[0] = dims[0];
+  shape[1] = dims[1];
+
+  return shape;
+}
+
+int main (int argc, char *argv[])
+{
+  TPS::Tps tps(argc, argv);
+  tps.parseCommandLineArgs(argc, argv);
+  tps.parseInput();
+  tps.chooseDevices();
+
+  // srand (time(NULL));
+  // double dummy = uniformRandomNumber();
+
+  M2ulPhyS *srcField = new M2ulPhyS(tps.getMPISession(), &tps);
+
+  RunConfiguration& srcConfig = srcField->GetConfig();
+  assert(srcConfig.GetWorkingFluid()==WorkingFluid::USER_DEFINED);
+  assert(srcConfig.GetGasModel()==PERFECT_MIXTURE);
+  assert(srcConfig.GetTranportModel()==ARGON_TERNARY);
+
+  int dim = 3;
+
+  Vector relErrorThreshold = Vector({2e-4, 2e-4, 3e-4});
+  Vector absErrorThreshold = Vector({5e-8, 4e-5, 1.5e-4});
+  int nTrials = 10;
+
+  double pressure = 101325.0;
+
+  PerfectMixture *mixture = new PerfectMixture( srcConfig, dim);
+  ArgonTernaryTransport *transport = new ArgonTernaryTransport(mixture, srcConfig);
+  int numSpecies = mixture->GetNumSpecies();
+  int numActiveSpecies = mixture->GetNumActiveSpecies();
+  int num_equation = mixture->GetNumEquations();
+  std::map<std::string, int> *speciesMapping = mixture->getSpeciesMapping();
+  int electronIndex = (*speciesMapping)["E"];
+  int ionIndex = (*speciesMapping)["Ar.+1"];
+  int neutralIndex = (*speciesMapping)["Ar"];
+
+  std::string fileName = "./ref_solns/transport/Chung.transport.h5";
+  std::string datasetName = "transport";
+  DenseMatrix refValues;
+  Array<int> dims1 = readTable(fileName, datasetName, refValues);
+  grvy_printf(GRVY_INFO, "\n T\t Viscosity \t Thermal cond. (H) \t Thermal cond. (E) \t Ion. degree \n");
+  for (int i = 0; i < dims1[0]; i++) {
+    for (int j = 0; j < dims1[1]; j++) {
+      std::cout << refValues(i,j) << ",\t";
+    }
+    std::cout << std::endl;
+  }
+
+  fileName = "./ref_solns/transport/Devoto1973.transport.h5";
+  DenseMatrix Devoto;
+  Array<int> dims2 = readTable(fileName, datasetName, Devoto);
+  Array<int> idxs(dims1[0]);
+  idxs[0] = 2;
+  idxs[1] = 3;
+  idxs[2] = 4;
+  idxs[3] = 5;
+  idxs[4] = 6;
+  idxs[5] = 7;
+  idxs[6] = 9;
+  idxs[7] = 11;
+  idxs[8] = 13;
+  idxs[9] = 15;
+  idxs[10] = 17;
+
+  Vector Tm(dims1[0]);
+  refValues.GetColumn(0, Tm);
+  Vector ionizationDegree(dims1[0]);
+  refValues.GetColumn(dims1[1] - 1, ionizationDegree);
+  // double error = 0.0, error1 = 0.0;
+  Vector error(3);
+  for (int i = 0; i < dims1[0]; i++) {
+    double nTotal = pressure / UNIVERSALGASCONSTANT / Tm(i);
+    double a = ionizationDegree(i);
+    Vector n_sp(3);
+    n_sp(electronIndex) = a / (1.0 + a) * nTotal;
+    n_sp(ionIndex) = n_sp(electronIndex);
+    n_sp(neutralIndex) = (1.0 - a) / (1.0 + a) * nTotal;
+    // Vector X_sp(3);
+    // std::cout << "X: ";
+    // for (int sp = 0; sp < 3; sp++) {
+    //   X_sp(sp) = n_sp(sp) / nTotal;
+    //   std::cout << X_sp(sp) << ",\t";
+    // }
+    // std::cout << std::endl;
+
+    double rho = 0.0;
+    for (int sp = 0; sp < 3; sp++) rho += n_sp(sp) * mixture->GetGasParams(sp, GasParams::SPECIES_MW);
+
+    Vector primitiveState(num_equation);
+    primitiveState = 0.0;
+    primitiveState(0) = rho;
+    primitiveState(1 + dim) = Tm(i);
+    for (int sp = 0; sp < numActiveSpecies; sp++) {
+      primitiveState(2 + dim + sp) = n_sp(sp);
+    }
+    // for (int eq = 0; eq < num_equation; eq++) {
+    //   std::cout << primitiveState(eq) << ",\t";
+    // }
+    // std::cout << n_sp(electronIndex) << ",\t" << n_sp(neutralIndex) << std::endl;
+
+    Vector conservedState(num_equation);
+    mixture->GetConservativesFromPrimitives(primitiveState, conservedState);
+
+    DenseMatrix gradUp(num_equation,3);
+    gradUp = 0.0;
+
+    Vector transportBuffer;
+    DenseMatrix diffusionVelocity;
+    transport->ComputeFluxTransportProperties(conservedState, gradUp, transportBuffer, diffusionVelocity);
+
+    // error = max(error, abs(refValues(i,1) - transportBuffer[GlobalTrnsCoeffs::VISCOSITY]) / refValues(i,1));
+    error(0) = abs(refValues(i,1) - transportBuffer[GlobalTrnsCoeffs::VISCOSITY]) / refValues(i,1);
+    if (error(0) > relErrorThreshold(0)) {
+      grvy_printf(GRVY_ERROR, "\n Viscosity deviates from the reference value.");
+      grvy_printf(GRVY_ERROR, "\n Relative Error: %.15E", error(0));
+      exit(ERROR);
+    }
+    error(1) = abs(refValues(i,2) - transportBuffer[GlobalTrnsCoeffs::HEAVY_THERMAL_CONDUCTIVITY]) / refValues(i,2);
+    if (error(1) > relErrorThreshold(1)) {
+      grvy_printf(GRVY_ERROR, "\n Heavy-species thermal conductivity deviates from the reference value.");
+      grvy_printf(GRVY_ERROR, "\n Relative Error: %.15E", error(1));
+      exit(ERROR);
+    }
+    if (i < 4) {
+      error(2) = abs(refValues(i,3) - transportBuffer[GlobalTrnsCoeffs::ELECTRON_THERMAL_CONDUCTIVITY]);
+      if (error(2) > absErrorThreshold(2)) {
+        grvy_printf(GRVY_ERROR, "\n Heavy-species thermal conductivity deviates from the reference value.");
+        grvy_printf(GRVY_ERROR, "\n Absolute Error: %.15E", error(1));
+        exit(ERROR);
+      }
+    } else {
+      error(2) = abs(refValues(i,3) - transportBuffer[GlobalTrnsCoeffs::ELECTRON_THERMAL_CONDUCTIVITY]) / refValues(i,3);
+      if (error(2) > relErrorThreshold(2)) {
+        grvy_printf(GRVY_ERROR, "\n Heavy-species thermal conductivity deviates from the reference value.");
+        grvy_printf(GRVY_ERROR, "\n Relative Error: %.15E", error(1));
+        exit(ERROR);
+      }
+    }
+
+  } // for i
+
+  grvy_printf(GRVY_INFO, "\n PASS: argon ternary mixture transport. \n");
+
+  return 0;
+}
