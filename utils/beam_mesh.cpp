bc3b6ab0c4592b0ec3459e8e22a1b8fd381435e9
dreamer2368 <seung.chung@austin.utexas.edu>
2022-02-24 15:33:53 -0600

    utils/beam_mesh: enabled selective (x or y) periodicity.

--- utils/beam_mesh.cpp
+++ utils/beam_mesh.cpp
@@ -43,27 +43,16 @@ static int    nx_    = 5;
 static int    nt_    = 3;
 static double a_     = 5.0;
 static double b_     = 1.0;
+static int    ser_ref_levels = 3;
+static int    periodic_dir = 2; // 0 for x only, 1 for y only, 2 for both
 // static double c_     = 3.0;
 
 // void pts(int iphi, int t, double x[]);
 // void trans(const Vector &x, Vector &p);
 
-template<typename T>
-bool InArray(const T* begin, size_t sz, T i)
-{
-   const T *end = begin + sz;
-   return std::find(begin, end, i) != end;
-}
-
-bool IndicesAreConnected(const Table &t, int i, int j)
-{
-   return InArray(t.GetRow(i), t.RowSize(i), j)
-          && InArray(t.GetRow(j), t.RowSize(j), i);
-}
-
 int main(int argc, char *argv[])
 {
-   int ser_ref_levels = 3;
+
    int el_type = 4;
    bool per_mesh = true;
    bool dg_mesh  = true;
@@ -82,8 +71,8 @@ int main(int argc, char *argv[])
                   "Width of the base in x-direction.");
    args.AddOption(&b_, "-b", "--base-y",
                   "Width of the base in y-direction.");
-   // args.AddOption(&c_, "-c", "--height",
-   //                "Height in z-direction.");
+   args.AddOption(&periodic_dir, "-dir", "--direction",
+                  "direction for periodicity.");
    args.AddOption(&el_type, "-e", "--element-type",
                   "Element type: 4 - Quadrilateral, 6 - Triangle.");
    // args.AddOption(&per_mesh, "-pm", "--periodic-mesh",
@@ -127,112 +116,31 @@ int main(int argc, char *argv[])
    Mesh orig_mesh = Mesh::MakeCartesian2D(nt_*nx_, nt_, el_type_, false, a_, b_, false);
 
    std::vector<int> v2v(orig_mesh.GetNV());
-   std::vector<Vector> translations = {Vector({a_,0.0}), Vector({0.0,b_})};
+   std::vector<Vector> translations;
+   switch (periodic_dir) {
+     case 0:
+       translations = {Vector({a_,0.0})};
+       break;
+     case 1:
+       translations = {Vector({0.0,b_})};
+       break;
+     case 2:
+       translations = {Vector({a_,0.0}), Vector({0.0,b_})};
+       break;
+     default:
+       cout << "Unsupported periodic direction" << endl;
+       exit(1);
+       break;
+   }
    v2v = orig_mesh.CreatePeriodicVertexMapping(translations);
    Mesh mesh = Mesh::MakePeriodic(orig_mesh,v2v);
    std::cout << "created periodic mesh." << std::endl;
-  //  assert(mesh.GetNV() == nx_ * 1);
-  // if (el_type_ == Element::QUADRILATERAL)
-  // {
-  //    const Table &e2e = mesh.ElementToElementTable();
-  //    // std::cout << e2e << std::endl;
-  //    for (int i=0; i<nx_*nt_; i++)
-  //    {
-  //       // Bottom row connected to top row
-  //       assert(IndicesAreConnected(e2e, i, i + nx_*nt_*(nt_-1)));
-  //     }
-  //     for (int i=0; i<nt_; i++){
-  //       // Left column connected to right column
-  //       assert(IndicesAreConnected(e2e, i*nx_*nt_, nx_*nt_-1 + i*nx_*nt_));
-  //    }
-  // }
 
   // Promote to high order mesh
   if (order_ > 1 || dg_mesh || per_mesh)
   {
      orig_mesh.SetCurvature(order_, dg_mesh || per_mesh, 2, Ordering::byVDIM);
   }
-
-   // if (nt_ != 0 )
-   // {
-   //    mesh.Transform(trans);
-   // }
-
-   // while (per_mesh)
-   // {
-   //
-   //    // Verify topological compatibility
-   //    if (nt_ % 2 == 1 && (el_type_ == Element::TETRAHEDRON ||
-   //                         el_type_ == Element::WEDGE))
-   //    {
-   //       cout << "Diagonal cuts on the base and top must line up "
-   //            << "for a periodic mesh!" << endl;
-   //       exit(1);
-   //    }
-   //
-   //    int nnode = 4;
-   //    int noff = (nt_ >= 0) ? 0 : (nnode * (1 - nt_ / nnode));
-   //
-   //    Array<int> v2v(mesh.GetNV());
-   //    for (int i = 0; i < v2v.Size() - nnode; i++)
-   //    {
-   //       v2v[i] = i;
-   //    }
-   //    // identify vertices at the extremes of the stack
-   //    switch ((noff + nt_) % nnode)
-   //    {
-   //       case 0:
-   //          v2v[v2v.Size() - nnode + 0] = 0;
-   //          v2v[v2v.Size() - nnode + 1] = 1;
-   //          v2v[v2v.Size() - nnode + 2] = 2;
-   //          v2v[v2v.Size() - nnode + 3] = 3;
-   //          break;
-   //       case 1:
-   //          v2v[v2v.Size() - nnode + 0] = 2;
-   //          v2v[v2v.Size() - nnode + 1] = 0;
-   //          v2v[v2v.Size() - nnode + 2] = 3;
-   //          v2v[v2v.Size() - nnode + 3] = 1;
-   //          break;
-   //       case 2:
-   //          v2v[v2v.Size() - nnode + 0] = 3;
-   //          v2v[v2v.Size() - nnode + 1] = 2;
-   //          v2v[v2v.Size() - nnode + 2] = 1;
-   //          v2v[v2v.Size() - nnode + 3] = 0;
-   //          break;
-   //       case 3:
-   //          v2v[v2v.Size() - nnode + 0] = 1;
-   //          v2v[v2v.Size() - nnode + 1] = 3;
-   //          v2v[v2v.Size() - nnode + 2] = 0;
-   //          v2v[v2v.Size() - nnode + 3] = 2;
-   //          break;
-   //    }
-   //    // renumber elements
-   //    for (int i = 0; i < mesh.GetNE(); i++)
-   //    {
-   //       Element *el = mesh.GetElement(i);
-   //       int *v = el->GetVertices();
-   //       int nv = el->GetNVertices();
-   //       for (int j = 0; j < nv; j++)
-   //       {
-   //          v[j] = v2v[v[j]];
-   //       }
-   //    }
-   //    // renumber boundary elements
-   //    for (int i = 0; i < mesh.GetNBE(); i++)
-   //    {
-   //       Element *el = mesh.GetBdrElement(i);
-   //       int *v = el->GetVertices();
-   //       int nv = el->GetNVertices();
-   //       for (int j = 0; j < nv; j++)
-   //       {
-   //          v[j] = v2v[v[j]];
-   //       }
-   //    }
-   //    mesh.RemoveUnusedVertices();
-   //    mesh.RemoveInternalBoundaries();
-   //
-   //    break;
-   // }
 
    // Refine the mesh if desired
    for (int lev = 0; lev < ser_ref_levels; lev++)
@@ -258,7 +166,17 @@ int main(int argc, char *argv[])
       }
       if (per_mesh)
       {
-         oss << "-p";
+        switch (periodic_dir) {
+          case 0:
+            oss << "-xp";
+            break;
+          case 1:
+            oss << "-yp";
+            break;
+          case 2:
+            oss << "-p";
+            break;
+        }
       }
       else if (dg_mesh)
       {
@@ -276,28 +194,6 @@ int main(int argc, char *argv[])
       ofs.close();
    }
 
-   // // Output the resulting mesh to GLVis
-   // if (visualization)
-   // {
-   //    char vishost[] = "localhost";
-   //    int  visport   = 19916;
-   //    socketstream sol_sock(vishost, visport);
-   //    sol_sock.precision(8);
-   //    sol_sock << "mesh\n" << mesh << flush;
-   // }
-
    // Clean up and exit
    return 0;
 }
-
-// void trans(const Vector &x, Vector &p)
-// {
-//    double z = x[2];
-//    double phi = 0.5 * M_PI * nt_ * z / c_;
-//    double cp = cos(phi);
-//    double sp = sin(phi);
-//
-//    p[0] = 0.5 * a_ + (x[0] - 0.5 * a_) * cp - (x[1] - 0.5 * b_) * sp;
-//    p[1] = 0.5 * b_ + (x[0] - 0.5 * a_) * sp + (x[1] - 0.5 * b_) * cp;
-//    p[2] = z;
-// }
