4c83e3924e9500f538c8cd957b0f85f968efe1c8
dreamer2368 <seung.chung@austin.utexas.edu>
2022-03-17 16:50:32 -0500

    Removing inlet BC dependency on dry air assumption

--- src/inletBC.cpp
+++ src/inletBC.cpp
@@ -686,7 +686,6 @@ void InletBC::subsonicNonReflectingDensityVelocity(Vector &normal, Vector &state
 
 // TODO: generalize for all fluid cases.
 void InletBC::subsonicReflectingDensityVelocity(Vector &normal, Vector &stateIn, Vector &bdrFlux) {
-  const double gamma = mixture->GetSpecificHeatRatio();
   const double p = mixture->ComputePressure(stateIn);
 
   Vector state2(num_equation);
@@ -694,13 +693,10 @@ void InletBC::subsonicReflectingDensityVelocity(Vector &normal, Vector &stateIn,
   state2[1] = inputState[0] * inputState[1];
   state2[2] = inputState[0] * inputState[2];
   if (nvel == 3) state2[3] = inputState[0] * inputState[3];
-  double k = 0.;
-  for (int d = 0; d < nvel; d++) k += state2[1 + d] * state2[1 + d];
-  state2[1 + nvel] = p / (gamma - 1.) + 0.5 * k / state2[0];
 
   if (eqSystem == NS_PASSIVE) state2[num_equation - 1] = 0.;
 
-  // mixture->GetConservativesFromPrimitives()
+  mixture->modifyEnergyForPressure(state2, state2, p);
 
   rsolver->Eval(stateIn, state2, normal, bdrFlux, true);
 }
