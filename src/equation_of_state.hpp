028baf97ad893ddf4cd6b24fa4628f90dbf5ccb9
Todd A. Oliver <oliver@oden.utexas.edu>
2022-05-09 09:52:53 -0500

    Refactor wall BCs in _GPU_ path to be valid on cpu

--- src/equation_of_state.hpp
+++ src/equation_of_state.hpp
@@ -330,6 +330,14 @@ class DryAir : public GasMixture {
     if (thrd == 1 + dim) stagState[thrd] = Rg / (gamma - 1.) * stateIn[0] * Temp;
   }
 
+  static MFEM_HOST_DEVICE void computeStagnantStateWithTemp_gpu_serial(const double *stateIn, double *stagState,
+                                                                       const double &Temp, const double &gamma,
+                                                                       const double &Rg, const int &num_equation,
+                                                                       const int &dim) {
+    for (int d = 0; d < dim; d++) stagState[1+d] = 0.;
+    stagState[1 + dim] = Rg / (gamma - 1.) * stateIn[0] * Temp;
+  }
+
   static MFEM_HOST_DEVICE void modifyEnergyForPressure_gpu(const double *stateIn, double *stateOut, const double &p,
                                                            const double &gamma, const double &Rg,
                                                            const int &num_equation, const int &dim, const int &thrd,
