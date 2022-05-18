43817651718c2ac2a32ac5c248836921787c7ca7
dreamer2368 <seung.chung@austin.utexas.edu>
2022-05-17 20:18:57 -0500

    save rhs for masa run.

--- src/M2ulPhyS.hpp
+++ src/M2ulPhyS.hpp
@@ -280,6 +280,7 @@ class M2ulPhyS : public TPS::Solver {
 
   ParGridFunction *zeroU_;  // to compute L2 norm of exact solution via ComputeLpError.
   ParGridFunction *masaU_;  // for visualization of the exact solution.
+  ParGridFunction *masaRhs_;  // for visualization of the right-hand side.
   BlockVector *zeroUBlock_, *masaUBlock_;
 #endif
 
