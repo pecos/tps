b7d18be5d6c53804fdbb02afe718656a69f46c2a
dreamer2368 <seung.chung@austin.utexas.edu>
2022-05-24 12:13:50 -0500

    commented out intOrder-- for Lobatto basis with square element. compute_rhs converges as it is supposed to.

--- src/gradients.cpp
+++ src/gradients.cpp
@@ -88,7 +88,7 @@ Gradients::Gradients(ParFiniteElementSpace *_vfes, ParFiniteElementSpace *_gradU
 
     // element volume integral
     int intorder = 2 * elem->GetOrder();
-    if (intRuleType == 1 && elem->GetGeomType() == Geometry::SQUARE) intorder--;  // when Gauss-Lobatto
+    // if (intRuleType == 1 && elem->GetGeomType() == Geometry::SQUARE) intorder--;  // when Gauss-Lobatto
     const IntegrationRule *ir = &intRules->Get(elem->GetGeomType(), intorder);
 
     Vector shape(eldDof);
--- src/sbp_integrators.cpp
+++ src/sbp_integrators.cpp
@@ -84,7 +84,7 @@ void SBPintegrator::AssembleElementVector(const FiniteElement& el, ElementTransf
 
   // Integration order calculation from DGTraceIntegrator
   int intorder = 2 * el.GetOrder();
-  if (el.GetGeomType() == Geometry::SQUARE) intorder--;  // when Gauss-Lobatto
+  // if (el.GetGeomType() == Geometry::SQUARE) intorder--;  // when Gauss-Lobatto
 
   const IntegrationRule* ir = &intRules->Get(Tr.GetGeometryType(), intorder);
 
