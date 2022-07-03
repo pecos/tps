98a65f0ac51965e1053246c2ca4e9c1f0b9b6eaa
dreamer2368 <seung.chung@austin.utexas.edu>
2022-07-02 20:23:48 -0500

    MFEM_HOST_DEVICE CurtissHirschfelder.

--- src/transport_properties.hpp
+++ src/transport_properties.hpp
@@ -122,6 +122,8 @@ class TransportProperties {
   // Curtiss-Hirschfelder approximation of diffusivity.
   void CurtissHirschfelder(const Vector &X_sp, const Vector &Y_sp, const DenseSymmetricMatrix &binaryDiff,
                            Vector &avgDiff);
+  MFEM_HOST_DEVICE void CurtissHirschfelder(const double *X_sp, const double *Y_sp, const double *binaryDiff,
+                                            double *avgDiff);
 };
 
 //////////////////////////////////////////////////////
