pecos/tps

commit 68ff89a028b90cd91f1000b91155fcac1c308413

Upgrade 2d conductivity interpolation to support higher order

src/cycle_avg_joule_coupling.cpp
@@ -76,22 +76,60 @@ void CycleAvgJouleCoupling::interpConductivityFromFlowToEM() {
   bool verbose = mpi_.Root();
   if (verbose) grvy_printf(ginfo, "Interpolating conductivity to EM mesh.\n");
   const ParMesh *em_mesh = qmsa_solver_->getMesh();
+  const ParFiniteElementSpace *em_fespace = qmsa_solver_->getFESpace();
 
-  // NB: Code below only valid if 1) conductivity treated as H1
-  // function, 2) order of conductivity field is same as the order of
-  // the mesh and 3) nodes for em mesh are ordered byNODES
-  //
-  // TODO(trevilo): Generalize beyond the constraints above
-  const Vector vxyz_em = *(em_mesh->GetNodes());
+  const int NE = em_mesh->GetNE();
+  const int nsp = em_fespace->GetFE(0)->GetNodes().GetNPoints();
   const int dim = em_mesh->Dimension();
-  const int n_mesh_nodes = vxyz_em.Size() / dim;
 
-  Vector conductivity_em(n_mesh_nodes);
+  // Generate list of points where the grid function will be evaluated.
+  Vector vxyz;
+
+  vxyz.SetSize(nsp * NE * dim);
+  for (int i = 0; i < NE; i++) {
+    const FiniteElement *fe = em_fespace->GetFE(i);
+    const IntegrationRule ir = fe->GetNodes();
+    ElementTransformation *et = em_fespace->GetElementTransformation(i);
+
+    DenseMatrix pos;
+    et->Transform(ir, pos);
+    Vector rowx(vxyz.GetData() + i * nsp, nsp);
+    Vector rowy(vxyz.GetData() + i * nsp + NE * nsp, nsp);
+    Vector rowz;
+    if (dim == 3) {
+      rowz.SetDataAndSize(vxyz.GetData() + i * nsp + 2 * NE * nsp, nsp);
+    }
+    pos.GetRow(0, rowx);
+    pos.GetRow(1, rowy);
+    if (dim == 3) {
+      pos.GetRow(2, rowz);
+    }
+  }
+  const int nodes_cnt = vxyz.Size() / dim;
+
+  // Interpolate
+  Vector conductivity_em(nodes_cnt);
   const ParGridFunction *conductivity_flow_gf = flow_solver_->GetPlasmaConductivityGF();
-  interp_flow_to_em_->Interpolate(vxyz_em, *conductivity_flow_gf, conductivity_em);
+  interp_flow_to_em_->Interpolate(vxyz, *conductivity_flow_gf, conductivity_em);
 
+  // Set grid function
   ParGridFunction *conductivity_em_gf = qmsa_solver_->getPlasmaConductivityGF();
-  *conductivity_em_gf = conductivity_em;
+
+  Array<int> vdofs;
+  Vector vals;
+  Vector elem_dof_vals(nsp);
+
+  for (int i = 0; i < NE; i++) {
+    em_fespace->GetElementVDofs(i, vdofs);
+    vals.SetSize(vdofs.Size());
+    for (int j = 0; j < nsp; j++) {
+      // Arrange values byNodes
+      elem_dof_vals(j) = conductivity_em(i*nsp + j);
+    }
+    conductivity_em_gf->SetSubVector(vdofs, elem_dof_vals);
+  }
+
+  conductivity_em_gf->SetFromTrueVector();
 }
 
 void CycleAvgJouleCoupling::interpJouleHeatingFromEMToFlow() {

test/inputs/interp-em-test.ini
@@ -86,8 +86,10 @@ viscosityRatio = 100.
 #---------------------------------------
 [em]
 mesh = meshes/inline-square-tri.mesh
-order       =  1                 # FE order (polynomial degree)
-ref_levels  =  1
+#order       =  1                 # FE order (polynomial degree)
+#ref_levels  =  1
+order       =  2                 # FE order (polynomial degree)
+ref_levels  =  0
 max_iter    =  1000              # max number of iterations
 rtol        =  1.0e-12           # solver relative tolerance
 atol        =  1.0e-18           # solver absolute tolerance

commit 655d16cea994878cee8ae486589d556b9318b258

Fix parallel bug in flow -> em interpolation
This commit fixes a bug introduced in 68ff89a0.  Specifically, before
calling SetFromTrueVector, we have to set the true vector by calling
SetTrueVector.  This was neglected before, which led to seg faults in
parallel.

src/cycle_avg_joule_coupling.cpp
@@ -138,6 +138,7 @@ void CycleAvgJouleCoupling::interpConductivityFromFlowToEM() {
     conductivity_em_gf->SetSubVector(vdofs, elem_dof_vals);
   }
 
+  conductivity_em_gf->SetTrueVector();
   conductivity_em_gf->SetFromTrueVector();
 #else
   mfem_error("Cannot interpolate without GSLIB support.");

test/interp_em.test
@@ -13,3 +13,9 @@ setup() {
     ./test_interp_em -run $RUNFILE_AXISYM
 }
 
+@test "[$TEST] flow/em interpolation: test 2d (axisymmetric) interpolation capability (2 mpi ranks)" {
+    test -s $RUNFILE_AXISYM
+    test -e ./test_interp_em
+    mpirun -np 2 ./test_interp_em -run $RUNFILE_AXISYM
+}
+