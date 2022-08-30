e7a309a7aea1cfe28afe53bffab47e65db547885
dreamer2368 <seung.chung@austin.utexas.edu>
2022-08-30 10:40:01 -0500

    findInterval test for values outside the range.

--- test/test_table.cpp
+++ test/test_table.cpp
@@ -14,44 +14,6 @@ const int nTrials = 10;
 double uniformRandomNumber() {
   return (double) rand() / (RAND_MAX);
 }
-//
-// Array<int> readTable(const string fileName, const string datasetName, DenseMatrix &output) {
-//   hid_t file = -1;
-//   if (file_exists(fileName)) {
-//     file = H5Fopen(fileName.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-//   } else {
-//     grvy_printf(GRVY_ERROR, "[ERROR]: Unable to open file -> %s\n", fileName.c_str());
-//     exit(ERROR);
-//   }
-//   assert(file >= 0);
-//
-//   hid_t datasetID, dataspace;
-//   datasetID = H5Dopen2(file, datasetName.c_str(), H5P_DEFAULT);
-//   assert(datasetID >= 0);
-//   dataspace = H5Dget_space(datasetID);
-//   const int ndims = H5Sget_simple_extent_ndims(dataspace);
-//   hsize_t dims[ndims];
-//   // int dummy = H5Sget_simple_extent_dims(dataspace,dims,NULL);
-//   H5Sget_simple_extent_dims(dataspace,dims,NULL);
-//
-//   // DenseMatrix memory is column-major, while HDF5 follows row-major.
-//   output.SetSize(dims[1], dims[0]);
-//
-//   herr_t status;
-//   status = H5Dread(datasetID, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, output.HostReadWrite());
-//   assert(status >= 0);
-//   H5Dclose(datasetID);
-//   H5Fclose(file);
-//
-//   // DenseMatrix memory is column-major, while HDF5 follows row-major.
-//   output.Transpose();
-//
-//   Array<int> shape(2);
-//   shape[0] = dims[0];
-//   shape[1] = dims[1];
-//
-//   return shape;
-// }
 
 int main (int argc, char *argv[])
 {
@@ -85,6 +47,19 @@ int main (int argc, char *argv[])
       grvy_printf(GRVY_ERROR, "findIndex failed!: %.5f < xEval: %.5f < %.5f\n", input.xdata[index], xEval, input.xdata[index+1]);
       exit(ERROR);
     }
+  }
+  // Check the values outside the range.
+  double xEval = input.xdata[0] - 0.1 * (input.xdata[Ndata-1] - input.xdata[0]);
+  int index = table->findInterval(xEval);
+  if (index != 0) {
+    grvy_printf(GRVY_ERROR, "findIndex below the range failed!: %d != %d\n", index, 0);
+    exit(ERROR);
+  }
+  xEval = input.xdata[Ndata-1] + 0.1 * (input.xdata[Ndata-1] - input.xdata[0]);
+  index = table->findInterval(xEval);
+  if (index != (Ndata-2)) {
+    grvy_printf(GRVY_ERROR, "findIndex above the range failed!: %d != %d\n", index, Ndata-2);
+    exit(ERROR);
   }
 
   delete table;
