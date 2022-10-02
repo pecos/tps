20d5f9582a9a90b28b2ad9f0cc6dd1977b9245a4
dreamer2368 <seung.chung@austin.utexas.edu>
2022-10-02 16:24:18 -0500

    add radiation.hpp/cpp...

---
+++ src/radiation.cpp
@@ -0,0 +1,67 @@
+// -----------------------------------------------------------------------------------bl-
+// BSD 3-Clause License
+//
+// Copyright (c) 2020-2022, The PECOS Development Team, University of Texas at Austin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// -----------------------------------------------------------------------------------el-
+
+#include "radiation.hpp"
+
+using namespace mfem;
+using namespace std;
+
+MFEM_HOST_DEVICE NetEmission::NetEmission(const RadiationInput &inputs) : Radiation() {
+  assert(inputs.model == NET_EMISSION);
+  switch (inputs.necModel) {
+    case TABULATED_NEC: {
+      switch (inputs.necTableInput.order) {
+        case 1: {
+          necTable_ = new LinearTable(inputs.necTableInput);
+        } break;
+        default: {
+          printf("NetEmission: given interpolation order is not supported for TableInterpolator!");
+          assert(false);
+        } break;
+      }
+      break;
+    }
+    default: {
+      printf("NetEmission: given coefficient model is not supported!");
+      assert(false);
+      break;
+    }
+  }
+}
+
+MFEM_HOST_DEVICE NetEmission::~NetEmission() {
+  if (necTable_ != NULL) delete necTable_;
+}
+
+MFEM_HOST_DEVICE double NetEmission::computeEnergySink(const double &T_h) {
+  return -4.0 * PI_ * necTable_->eval(T_h);
+}
