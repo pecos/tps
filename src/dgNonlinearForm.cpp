016016d368195a5da79c9f40c34fb2f2a5d248e6
Todd A. Oliver <oliver@oden.utexas.edu>
2022-11-15 08:43:35 -0800

    don't use static functions in dgNonlinearForm.cpp

--- src/dgNonlinearForm.cpp
+++ src/dgNonlinearForm.cpp
@@ -361,19 +361,22 @@ void DGNonLinearForm::evalFaceFlux_gpu() {
                          d_shapeWnor1 + offsetShape1 + maxDofs + 1 + k * (maxDofs + 1 + dim),
                          Rflux);
 
-#if defined(_CUDA_)
-      // TODO(kevin): implement radius.
       d_flux->ComputeViscousFluxes(u1, gradUp1, 0.0, vFlux1);
       d_flux->ComputeViscousFluxes(u2, gradUp2, 0.0, vFlux2);
-      // d_flux->ComputeViscousFluxes(d_uk_el1 + k * num_equation + iface * maxIntPoints * num_equation,
-      //             d_grad_uk_el1 + k * dim * num_equation + iface * maxIntPoints * dim * num_equation,
-      //             0.0, vFlux1);
-#elif defined(_HIP_)
-      Fluxes::viscousFlux_serial_gpu(&vFlux1[0], &u1[0], &gradUp1[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
-                                     num_equation);
-      Fluxes::viscousFlux_serial_gpu(&vFlux2[0], &u2[0], &gradUp2[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
-                                     num_equation);
-#endif
+
+// #if defined(_CUDA_)
+//       // TODO(kevin): implement radius.
+//       d_flux->ComputeViscousFluxes(u1, gradUp1, 0.0, vFlux1);
+//       d_flux->ComputeViscousFluxes(u2, gradUp2, 0.0, vFlux2);
+//       // d_flux->ComputeViscousFluxes(d_uk_el1 + k * num_equation + iface * maxIntPoints * num_equation,
+//       //             d_grad_uk_el1 + k * dim * num_equation + iface * maxIntPoints * dim * num_equation,
+//       //             0.0, vFlux1);
+// #elif defined(_HIP_)
+//       Fluxes::viscousFlux_serial_gpu(&vFlux1[0], &u1[0], &gradUp1[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
+//                                      num_equation);
+//       Fluxes::viscousFlux_serial_gpu(&vFlux2[0], &u2[0], &gradUp2[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
+//                                      num_equation);
+// #endif
       for (int d = 0; d < dim; d++) {
         for (int eq = 0; eq < num_equation; eq++) {
           vFlux1[eq + d * num_equation] = 0.5 * (vFlux1[eq + d * num_equation] + vFlux2[eq + d * num_equation]);
@@ -705,18 +708,23 @@ void DGNonLinearForm::sharedFaceInterpolation_gpu(const Vector &x) {
         }
 
         // evaluate flux
-#if defined(_CUDA_)
         // TODO(kevin): implement radius.
         d_rsolver->Eval_LF(u1, u2, nor, Rflux);
         d_flux->ComputeViscousFluxes(u1, gradUp1, 0.0, vFlux1);
         d_flux->ComputeViscousFluxes(u2, gradUp2, 0.0, vFlux2);
-#elif defined(_HIP_)
-        RiemannSolver::riemannLF_serial_gpu(&u1[0], &u2[0], &Rflux[0], &nor[0], gamma, Rg, dim, num_equation);
-        Fluxes::viscousFlux_serial_gpu(&vFlux1[0], &u1[0], &gradUp1[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
-                                       num_equation);
-        Fluxes::viscousFlux_serial_gpu(&vFlux2[0], &u2[0], &gradUp2[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
-                                       num_equation);
-#endif
+
+// #if defined(_CUDA_)
+//         // TODO(kevin): implement radius.
+//         d_rsolver->Eval_LF(u1, u2, nor, Rflux);
+//         d_flux->ComputeViscousFluxes(u1, gradUp1, 0.0, vFlux1);
+//         d_flux->ComputeViscousFluxes(u2, gradUp2, 0.0, vFlux2);
+// #elif defined(_HIP_)
+//         RiemannSolver::riemannLF_serial_gpu(&u1[0], &u2[0], &Rflux[0], &nor[0], gamma, Rg, dim, num_equation);
+//         Fluxes::viscousFlux_serial_gpu(&vFlux1[0], &u1[0], &gradUp1[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
+//                                        num_equation);
+//         Fluxes::viscousFlux_serial_gpu(&vFlux2[0], &u2[0], &gradUp2[0], gamma, Rg, viscMult, bulkViscMult, Pr, dim,
+//                                        num_equation);
+// #endif
 
         for (int eq = 0; eq < num_equation; eq++) {
           for (int d = 0; d < dim; d++)
