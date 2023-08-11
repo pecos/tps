8f016abe7a9380c4bb9d9120a377e5129113edb3
Todd A. Oliver <oliver@oden.utexas.edu>
2023-08-11 09:02:15 -0500

    Apply enforcestyle
    NB: The style check / enforcement is not automatically applied outside
    src dir (e.g., to utils).  So, applied clang-format by hand here.
    Should automate this.

--- utils/pfield_interpolate.cpp
+++ utils/pfield_interpolate.cpp
@@ -1,18 +1,16 @@
 /* A utility to transfer tps solutions from one mesh to another */
 
-#include "tps.hpp"
 #include "M2ulPhyS.hpp"
 #include "mfem.hpp"
+#include "tps.hpp"
 #include <fstream>
 
 using namespace mfem;
 using namespace std;
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   mfem::Mpi::Init(argc, argv);
   TPS::Tps tps(MPI_COMM_WORLD);
-
 
   // Set the method's default parameters.
   const char *src_input_file = "coarse.run";
@@ -26,11 +24,10 @@ int main (int argc, char *argv[])
                  "runFile for target case");
 
   args.Parse();
-  if (!args.Good())
-    {
-      args.PrintUsage(cout);
-      return 1;
-    }
+  if (!args.Good()) {
+    args.PrintUsage(cout);
+    return 1;
+  }
   args.PrintOptions(cout);
 
   // Instantiate M2ulPhyS classes for *both* the coarse and fine
@@ -44,15 +41,14 @@ int main (int argc, char *argv[])
   tps.parseInputFile(srcFileName);
   tps.chooseDevices();
 
-  M2ulPhyS srcField( srcFileName, &tps );
-  RunConfiguration& srcConfig = srcField.GetConfig();
-  assert(srcConfig.GetRestartCycle()>0);
+  M2ulPhyS srcField(srcFileName, &tps);
+  RunConfiguration &srcConfig = srcField.GetConfig();
+  assert(srcConfig.GetRestartCycle() > 0);
 
   tps.closeInputFile();
 
-  ParMesh* mesh_1 = srcField.getMesh();
+  ParMesh *mesh_1 = srcField.getMesh();
   const int dim = mesh_1->Dimension();
-
 
   // but, we require that RESTART_CYCLE is *not* set in the
   // target run file, since the target restart files do not exist yet.
@@ -60,29 +56,33 @@ int main (int argc, char *argv[])
 
   tps.parseInputFile(tarFileName);
 
-  M2ulPhyS tarField( tarFileName, &tps );
-  RunConfiguration& tarConfig = tarField.GetConfig();
-  assert(tarConfig.GetRestartCycle()==0);
+  M2ulPhyS tarField(tarFileName, &tps);
+  RunConfiguration &tarConfig = tarField.GetConfig();
+  assert(tarConfig.GetRestartCycle() == 0);
 
   tps.closeInputFile();
 
   // Get meshes
-  ParMesh* mesh_2 = tarField.getMesh();
+  ParMesh *mesh_2 = tarField.getMesh();
 
-  //const int dim = mesh_1->Dimension();
+  // const int dim = mesh_1->Dimension();
 
   // GSLIB only works in 2 and 3 dimensions
-  assert(dim>1);
+  assert(dim > 1);
   // Input meshes must have same dimension
-  assert(mesh_2->Dimension()==dim);
+  assert(mesh_2->Dimension() == dim);
 
-  if (mesh_1->GetNodes() == NULL) { mesh_1->SetCurvature(1); }
-  if (mesh_2->GetNodes() == NULL) { mesh_2->SetCurvature(1); }
+  if (mesh_1->GetNodes() == NULL) {
+    mesh_1->SetCurvature(1);
+  }
+  if (mesh_2->GetNodes() == NULL) {
+    mesh_2->SetCurvature(1);
+  }
 
-  cout << "Source mesh curvature: "
-       << mesh_1->GetNodes()->OwnFEC()->Name() << endl
-       << "Target mesh curvature: "
-       << mesh_2->GetNodes()->OwnFEC()->Name() << endl;
+  cout << "Source mesh curvature: " << mesh_1->GetNodes()->OwnFEC()->Name()
+       << endl
+       << "Target mesh curvature: " << mesh_2->GetNodes()->OwnFEC()->Name()
+       << endl;
 
   // 2) Set up source field
   const FiniteElementCollection *src_fec = NULL;
@@ -94,16 +94,17 @@ int main (int argc, char *argv[])
   // 3) Some checks
   const Geometry::Type gt = mesh_2->GetNodalFESpace()->GetFE(0)->GetGeomType();
   MFEM_VERIFY(gt != Geometry::PRISM, "Wedge elements are not currently "
-              "supported.");
-  MFEM_VERIFY(mesh_2->GetNumGeometries(mesh_2->Dimension()) == 1, "Mixed meshes"
+                                     "supported.");
+  MFEM_VERIFY(mesh_2->GetNumGeometries(mesh_2->Dimension()) == 1,
+              "Mixed meshes"
               "are not currently supported.");
 
   std::cout << "Source FE collection: " << src_fec->Name() << std::endl;
 
   // Setup the FiniteElementSpace and GridFunction on the target mesh.
-  const FiniteElementCollection* tar_fec = tarField.getFEC();
-  ParFiniteElementSpace* tar_fes = tarField.getFESpace();
-  ParGridFunction* func_target = tarField.GetSolutionGF();
+  const FiniteElementCollection *tar_fec = tarField.getFEC();
+  ParFiniteElementSpace *tar_fes = tarField.getFESpace();
+  ParGridFunction *func_target = tarField.GetSolutionGF();
   std::cout << "Target FE collection: " << tar_fec->Name() << std::endl;
 
   const int NE = mesh_2->GetNE();
@@ -115,7 +116,7 @@ int main (int argc, char *argv[])
   Vector vxyz;
 
   // NB: We assume DG here!
-  vxyz.SetSize(nsp*NE*dim);
+  vxyz.SetSize(nsp * NE * dim);
   for (int i = 0; i < NE; i++) {
     const FiniteElement *fe = tar_fes->GetFE(i);
     const IntegrationRule ir = fe->GetNodes();
@@ -123,20 +124,22 @@ int main (int argc, char *argv[])
 
     DenseMatrix pos;
     et->Transform(ir, pos);
-    Vector rowx(vxyz.GetData() + i*nsp, nsp);
-    Vector rowy(vxyz.GetData() + i*nsp + NE*nsp, nsp);
+    Vector rowx(vxyz.GetData() + i * nsp, nsp);
+    Vector rowy(vxyz.GetData() + i * nsp + NE * nsp, nsp);
     Vector rowz;
     if (dim == 3) {
-      rowz.SetDataAndSize(vxyz.GetData() + i*nsp + 2*NE*nsp, nsp);
+      rowz.SetDataAndSize(vxyz.GetData() + i * nsp + 2 * NE * nsp, nsp);
     }
     pos.GetRow(0, rowx);
     pos.GetRow(1, rowy);
-    if (dim == 3) { pos.GetRow(2, rowz); }
+    if (dim == 3) {
+      pos.GetRow(2, rowz);
+    }
   }
   const int nodes_cnt = vxyz.Size() / dim;
 
   // Evaluate source grid function.
-  Vector interp_vals(nodes_cnt*tar_ncomp);
+  Vector interp_vals(nodes_cnt * tar_ncomp);
   FindPointsGSLIB finder(MPI_COMM_WORLD);
   finder.Setup(*mesh_1);
   finder.Interpolate(vxyz, *func_source, interp_vals);
