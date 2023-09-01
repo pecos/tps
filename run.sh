d952f01e54b8998899c3ff2ca697de9bcb1299d6
Malamas Tsagkaridis <42969991+malamast@users.noreply.github.com>
2023-09-01 14:34:47 -0500

    Added an assert statement to make sure that exitCheckFreq gets only positive values.

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ cd test
 
 EXE=../src/tps
 # RUNFILE="inputs/input.malamas.test.ini"
-# RUNFILE="inputs/plasma.ini"
-RUNFILE="inputs/input.plasma.nlte.test.ini"
+RUNFILE="inputs/plasma.ini"
+# RUNFILE="inputs/input.plasma.nlte.test.ini"
 
 
 NPROC=2
