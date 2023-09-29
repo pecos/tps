ed52aa6d0efb9ec811c214988859f68e8c1cd578
Sigfried Haering <shaering@oden.utexas.edu>
2023-09-29 02:51:57 -0700

    numerous small changes, still an issue with plotting of total visc

--- src/run_configuration.hpp
+++ src/run_configuration.hpp
@@ -94,6 +94,10 @@ class RunConfiguration {
   // variable/constant time-step
   bool constantTimeStep;
   double dt_fixed;
+  double dt_initial;
+  double dt_factor;
+  int solver_iter;
+  double solver_tol;
 
   // num iterations. Defaults to 0
   int numIters;
@@ -188,7 +192,8 @@ class RunConfiguration {
   std::vector<pair<int, InletType>> inletPatchType;
   double velInlet[3];
   double tempInlet;
-  double densInlet;    
+  double densInlet;
+  int rampStepsInlet;
 
   // Outlet BC data
   Array<double> outletBC;
