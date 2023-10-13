5daa928467c06420e9ab2d9014d91c78989e3435
Umberto Villa <uvilla@oden.utexas.edu>
2023-10-13 09:26:58 -0500

    More modular build of Docker cuda containers;\n Sniff mpirun for portability on clusters

---
+++ test/sniff_mpirun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# PURPOSE understand how to run parallel jobs on HPC clusters
+
+found=1
+
+# defaut to mpirun if available
+MPIRUN=$(type -P mpirun) || found=0
+
+# if not, try flux
+if [ $found -eq 0 ]; then
+    type -P flux && MPIRUN="$(type -P flux) run" || found=0
+fi
+
+# if neither mpirun or flux found, then MPIRUN is empty and tests can be skipped using this
+#[ "x$MPIRUN" != "x" ] || skip "Cannot launch parallel job"
+
+echo $MPIRUN
