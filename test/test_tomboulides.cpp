1d1a14132d3b3139c35bc53e2a7423c097c03d0d
Todd A. Oliver <oliver@oden.utexas.edu>
2024-02-09 15:54:19 -0600

    Refactor Tomboulides test program
    So that it solves the 2d Taylor Green vortex, just as a first test.

--- test/test_tomboulides.cpp
+++ test/test_tomboulides.cpp
@@ -1,4 +1,5 @@
 #include <limits>
+#include <math.h>
 
 #include <mfem/general/forall.hpp>
 
@@ -9,108 +10,249 @@
 
 using namespace mfem;
 
+/// Used to set the velocity IC (and to check error)
+void vel_exact_tg(const Vector &x, double t, Vector &u) {
+  const double nu = 1.0;
+  const double F = std::exp(-2 * nu * t);
+
+  u(0) =  F * std::sin(x[0]) * std::cos(x[1]);
+  u(1) = -F * std::cos(x[0]) * std::sin(x[1]);
+}
+
+void updateTimeIntegrationCoefficients(int step, double *dthist, timeCoefficients &time_coeff) {
+  using std::pow;
+
+  // Maximum BDF order to use at current time step
+  // step + 1 <= order <= max_bdf_order
+  int bdf_order = std::min(step + 1, 3);
+
+  // Ratio of time step history at dt(t_{n}) - dt(t_{n-1})
+  double rho1 = 0.0;
+
+  // Ratio of time step history at dt(t_{n-1}) - dt(t_{n-2})
+  double rho2 = 0.0;
+
+  rho1 = dthist[0] / dthist[1];
+
+  if (bdf_order == 3) {
+    rho2 = dthist[1] / dthist[2];
+  }
+
+  if (step == 0 && bdf_order == 1) {
+    time_coeff.bd0 = 1.0;
+    time_coeff.bd1 = -1.0;
+    time_coeff.bd2 = 0.0;
+    time_coeff.bd3 = 0.0;
+    time_coeff.ab1 = 1.0;
+    time_coeff.ab2 = 0.0;
+    time_coeff.ab3 = 0.0;
+  } else if (step >= 1 && bdf_order == 2) {
+    time_coeff.bd0 = (1.0 + 2.0 * rho1) / (1.0 + rho1);
+    time_coeff.bd1 = -(1.0 + rho1);
+    time_coeff.bd2 = pow(rho1, 2.0) / (1.0 + rho1);
+    time_coeff.bd3 = 0.0;
+    time_coeff.ab1 = 1.0 + rho1;
+    time_coeff.ab2 = -rho1;
+    time_coeff.ab3 = 0.0;
+  } else if (step >= 2 && bdf_order == 3) {
+    time_coeff.bd0 = 1.0 + rho1 / (1.0 + rho1) + (rho2 * rho1) / (1.0 + rho2 * (1 + rho1));
+    time_coeff.bd1 = -1.0 - rho1 - (rho2 * rho1 * (1.0 + rho1)) / (1.0 + rho2);
+    time_coeff.bd2 = pow(rho1, 2.0) * (rho2 + 1.0 / (1.0 + rho1));
+    time_coeff.bd3 = -(pow(rho2, 3.0) * pow(rho1, 2.0) * (1.0 + rho1)) / ((1.0 + rho2) * (1.0 + rho2 + rho2 * rho1));
+    time_coeff.ab1 = ((1.0 + rho1) * (1.0 + rho2 * (1.0 + rho1))) / (1.0 + rho2);
+    time_coeff.ab2 = -rho1 * (1.0 + rho2 * (1.0 + rho1));
+    time_coeff.ab3 = (pow(rho2, 2.0) * rho1 * (1.0 + rho1)) / (1.0 + rho2);
+  }
+}
+
+
+
 /**
  * @brief Test that the interface design for the low Mach split time
- * integrators builds and runs a trivial program.
+ * integrators builds and runs.  For these purposes, we solve a 2D
+ * Taylor-Green problem.  Specifically, the domain is
+ *
+ * \Omega = (0, 2\pi) \times (0, 2\pi).
+ *
+ * The solution is
+ *
+ * u(x,y) = F(t) sin(x) cos(y),  v(x,y) = -F(t) cos(x) sin(y),
+ *
+ * where
+ *
+ * F(t) = exp(-2 \nu t),
+ *
+ * and \nu is the kinematic viscosity.
  */
 int main(int argc, char *argv[]) {
   // Initialize mpi
   Mpi::Init(argc, argv);
+  Hypre::Init();
 
-  // Create a mesh (doesn't really matter what it is)
-  Mesh *mesh = new Mesh("./meshes/cyl-tet-coarse.msh");
-  ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
+  int ref_levels = 2;
+  int order = 3;
+  double dt = -1.0;
+  double t_final = 1.0;
+
+  //---------------------------------------------------------------------
+  // Parse command line options
+  OptionsParser args(argc, argv);
+  args.AddOption(&ref_levels, "-r", "--refine",
+                 "Number of times to refine the mesh uniformly.");
+  args.AddOption(&order, "-o", "--order", "Order (degree) of the finite elements.");
+  args.AddOption(&dt, "-dt", "--time-step", "Time step.");
+  args.AddOption(&t_final, "-tf", "--final-time", "Final time.");
+
+  args.Parse();
+  if (!args.Good()) {
+    if (Mpi::Root()) {
+      args.PrintUsage(mfem::out);
+    }
+    return 1;
+  }
+  if (Mpi::Root()) {
+    args.PrintOptions(mfem::out);
+  }
+
+  // Create the mesh
+  Mesh *mesh = new Mesh("./meshes/inline-quad.mesh");
+
+  // Make it periodic
+  Vector x_translation({1.0, 0.0});
+  Vector y_translation({0.0, 1.0});
+  std::vector<Vector> translations = {x_translation, y_translation};
+
+  Mesh periodic_mesh = Mesh::MakePeriodic(*mesh,mesh->CreatePeriodicVertexMapping(translations));
+
+  // Scale domain to (0,L)x(0,L)
+  periodic_mesh.EnsureNodes();
+  GridFunction *nodes = periodic_mesh.GetNodes();
+  *nodes *= (2 * M_PI);
+
+  // Refine the periodic mesh
+  for (int i = 0; i < ref_levels; ++i) {
+    periodic_mesh.UniformRefinement();
+  }
+
+  // And finally, decompose it
+  ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, periodic_mesh);
   delete mesh;
 
+  // Get minimum element size
+  double hmin;
+  double local_hmin = 1.0e18;
+  for (int i = 0; i < pmesh->GetNE(); i++) {
+    local_hmin = std::min(pmesh->GetElementSize(i, 1), local_hmin);
+  }
+  MPI_Allreduce(&local_hmin, &hmin, 1, MPI_DOUBLE, MPI_MIN, pmesh->GetComm());
+
   // polynomial orders
-  const int sorder = 2;
-  const int vorder = 3;
-  const int porder = 3;
+  const int sorder = order;
+  const int vorder = order;
+  const int porder = order;
 
   // time step coefficients (1st order)
   timeCoefficients time_coeff;
   time_coeff.time = 0.0;
-  time_coeff.dt = 1.0;
-  time_coeff.ab1 = 1.0;
-  time_coeff.ab2 = 0.0;
-  time_coeff.ab3 = 0.0;
-  time_coeff.bd0 = 1.0;
-  time_coeff.bd1 = -1.0;
-  time_coeff.bd2 = 0.0;
-  time_coeff.bd3 = 0.0;
+  if (dt > 0.0) {
+    time_coeff.dt = dt;
+  } else {
+    time_coeff.dt = 0.5 * hmin / vorder; // max velocity magn is 1
+  }
+
+  double dthist[3];
+  dthist[0] = dthist[1] = dthist[2] = time_coeff.dt;
+  updateTimeIntegrationCoefficients(0, dthist, time_coeff);
+
 
   // Density and viscosity to use
-  const double rho = 42.0;
-  const double mu = 1.0e-6;
+  const double rho = 1.0;
+  const double mu = 1.0;
 
   // Set up the flow and thermo classes
   FlowBase *flow;
   ThermoChemModelBase *thermo;
 
+  // Instantiate model classes
   flow = new Tomboulides(pmesh, vorder, porder, time_coeff);
   thermo = new ConstantPropertyThermoChem(pmesh, sorder, rho, mu);
 
+  // Initialize -- after this, solution fields have been allocated
   flow->initializeSelf();
   thermo->initializeSelf();
+
+  // Get the velocity field and set the IC
+  ParGridFunction *u_gf = flow->getCurrentVelocity();
+  VectorFunctionCoefficient u_excoeff(2, vel_exact_tg);
+  u_excoeff.SetTime(0.0);
+  u_gf->ProjectCoefficient(u_excoeff);
 
 #if 0
   // Set above to 1 to ensure that assert catches uninitialized interface
   flow->initializeOperators();
 #endif
 
+  // Exchange interface information
   flow->initializeFromThermoChem(&thermo->interface);
   thermo->initializeFromFlow(&flow->interface);
 
+  // And now we can finish initializing the flow
   flow->initializeOperators();
 
-  // Take a time step (actually does nothing for these classes, but still)
-  thermo->step();
-  flow->step();
+  // Write IC to paraview
+  ParaViewDataCollection *pd = nullptr;
+  pd = new ParaViewDataCollection("tomboulides_tg_res", pmesh);
+  pd->SetPrefixPath("./");
+  pd->RegisterField("velocity", u_gf);
+  // pd->RegisterField("pressure", p_gf);
+  pd->SetLevelsOfDetail(vorder);
+  pd->SetDataFormat(VTKFormat::BINARY);
+  pd->SetHighOrderOutput(true);
+  pd->SetCycle(0);
+  pd->SetTime(time_coeff.time);
+  pd->Save();
 
-  time_coeff.time += time_coeff.dt;
-
-  flow->step();
-
-  // Make sure when we get the density via the flow interface it is correct
-  const ParGridFunction *rho_field = flow->getThermoInterface()->density;
-
-  const double tol = std::numeric_limits<double>::epsilon();
-  int ierr = 0;
-  {
-    Vector trho;
-    rho_field->GetTrueDofs(trho);
-    const double *d_rho = trho.Read();
-    double err = 0.0;
-    double *perr = &err;
-    MFEM_FORALL(i, trho.Size(), { perr[0] += abs(d_rho[i] - 42.0); });
-    if (std::abs(err) > tol) {
-      ierr += 1;
+  // Time march to t = 1.0
+  int iter = 0;
+  while (time_coeff.time < t_final) {
+    if (Mpi::Root()) {
+      std::cout << "Step " << iter << ": time = " << time_coeff.time << std::endl;
     }
+    // Take a time step
+    thermo->step();  // Thermo does nothing here
+    flow->step();
+    time_coeff.time += time_coeff.dt;
+    iter++;
+
+    // update dt history
+    dthist[2] = dthist[1];
+    dthist[1] = dthist[0];
+    dthist[0] = time_coeff.dt;
+
+    // update coefficients (dt is constant here)
+    updateTimeIntegrationCoefficients(iter, dthist, time_coeff);
+  }
+  if (Mpi::Root()) {
+    std::cout << "Final step " << iter << ": time = " << time_coeff.time << std::endl;
   }
 
-  // Make sure when we get the velocity via the thermo interface it is correct
-  if (thermo->getFlowInterface()->swirl_supported) {
-    ierr += 1;
+  u_excoeff.SetTime(time_coeff.time);
+  const double err_u = u_gf->ComputeL2Error(u_excoeff);
+  if (Mpi::Root()) {
+    std::cout << "Velocity error at final time = " << err_u << std::endl;
   }
 
-  const ParGridFunction *u_field = thermo->getFlowInterface()->velocity;
 
-  {
-    Vector tu;
-    u_field->GetTrueDofs(tu);
-    const double *d_u = tu.Read();
-    double err = 0.0;
-    double *perr = &err;
-    MFEM_FORALL(i, tu.Size(), { perr[0] += abs(d_u[i]); });
-    if (std::abs(err) > tol) {
-      ierr += 1;
-    }
-  }
+  // Write some visualization files
+  pd->SetCycle(1);
+  pd->SetTime(time_coeff.time);
+  pd->Save();
 
   // clean up
+  delete pd;
   delete thermo;
   delete flow;
   delete pmesh;
 
-  return ierr;
+  return 0;
 }
