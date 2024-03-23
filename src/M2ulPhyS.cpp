d8df437f115ad4b1c8b8543133d92c31aa7da371
Todd A. Oliver <oliver@oden.utexas.edu>
2024-03-23 14:52:20 -0500

    Fix U->HostWrite that should be U->HostRead in M2ulPhyS::updatePrimitives

--- src/M2ulPhyS.cpp
+++ src/M2ulPhyS.cpp
@@ -4019,8 +4019,7 @@ void M2ulPhyS::checkSolverOptions() const {
 }
 
 void M2ulPhyS::updatePrimitives() {
-  // U.V.: should this be U->HostRead() instead? U->HostWrite() does not sync memory before returning the pointer.
-  double *data = U->HostWrite();
+  const double *data = U->HostRead();
   double *dataUp = Up->HostWrite();
   int dof = vfes->GetNDofs();
 
