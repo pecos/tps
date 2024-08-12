Separated numerical integrals into linear and radial, and fixed an overwriting error in the implementation.
Changed how the LineInterpolator is implemented in the interface
and fixed a memory leak due to internal data remaining in FindPointsGSLib.

Added additional methods to control the interface behavior from Python.

Added various comments and descriptions to methods visible from Python.

src/flow1d-coupling.py
@@ -15,26 +15,33 @@ tps.parseCommandLineArgs(sys.argv)
 tps.parseInput()
 tps.chooseDevices()
 
-print('-'*40, '\nCreating 1d interface instance\n', '-'*40)
+# 1d flow - 2d EM interface
 interface = libtps.Qms2Flow1d(tps)
-print('-'*40, '\nInitializing 1d interface vector size\n', '-'*40)
-n_points = 25
+# Initialize MFEM vectors with number of 1d points
+n_points = 1000
 interface.initialize(n_points)
-interface.print_all()
 
-print('-'*40, '\nCreating NumPy array interfaces for vectors\n', '-'*40)
+# Access vectors through NumPy
 cond_1d = np.array(interface.PlasmaConductivity1d(), copy = False)
 joule_1d = np.array(interface.JouleHeating1d(), copy = False)
 radius_1d = np.array(interface.TorchRadius1d(), copy = False)
 z_1d = np.array(interface.Coordinates1d(), copy = False)
 
-R_TORCH = 2.75e-1
-print('-'*40, '\nSetting vector values\n', '-'*40)
-z_1d[0:n_points] = np.linspace(0, 0.5, n_points)
+# Example vector data
+R_TORCH = 2.75e-2
+z_1d[0:n_points] = np.linspace(0, 0.315, n_points)
 cond_1d[0:n_points] = np.ones(n_points)
-radius_1d[0:n_points] = R_TORCH*(1 - 0.1*np.exp(-((z_1d - 0.25)/0.05)**2))
-interface.print_all()
+radius_1d[0:n_points] = R_TORCH
 
-print('-'*40, '\nSolving EM field\n', '-'*40)
+# Solve
+interface.set_n_interp(100)
 interface.solve()
-interface.print_all()
+
+# Evaluate Joule heating
+tot_1d = interface.total_joule_1d()/1e3
+tot_2d = interface.total_joule_2d()/1e3
+error = 100*np.abs(tot_1d - tot_2d)/np.abs(tot_2d)
+
+print(f'Total 2d Joule heating [kW]: {tot_2d:.2f}')
+print(f'Total 1d Joule heating [kW]: {tot_1d:.2f}')
+print(f'Error [%]: {error:.3e}')