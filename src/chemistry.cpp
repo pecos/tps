5d61971a856d0a627847c0fefba0d3f83bd0e7d2
Sigfried Haering <shaering@oden.utexas.edu>
2024-10-15 02:07:43 -0700

    fix to radDecay test input

--- src/chemistry.cpp
+++ src/chemistry.cpp
@@ -94,12 +94,12 @@ MFEM_HOST_DEVICE Chemistry::Chemistry(GasMixture *mixture, const ChemistryInput 
       case RADIATIVE_DECAY: {
         assert(inputs.reactionInputs[r].modelParams != NULL);
 #ifndef _GPU_
-        double R = inputs.reactionInputs[r].modelParams[0];	
+        double R = inputs.reactionInputs[r].modelParams[0];
         reactions_[r] = new RadiativeDecay(R, &inputs.speciesMapping, &inputs.speciesNames, &numSpecies_,
                                            &reactantStoich_[r * numSpecies_], &productStoich_[r * numSpecies_]);
 #else
         printf("Radiative decay not currently supported for GPU execution.");
-	assert(false);
+        assert(false);
         break;
 #endif
       } break;
