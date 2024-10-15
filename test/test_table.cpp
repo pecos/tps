0af912b89745d80cddd741bd9243a5cea2ed74b0
Sigfried Haering <shaering@oden.utexas.edu>
2024-10-14 17:53:42 -0700

    reinstate tabel_test and update radDecay ref soln

--- test/test_table.cpp
+++ test/test_table.cpp
@@ -106,7 +106,8 @@ void testTableInterpolator1D(TPS::Tps &tps, int rank) {
     double xtest = refValues(k, 0);
     double fref = refValues(k, 1);
     double ftest[gpudata::MAXREACTIONS];
-    chem->computeForwardRateCoeffs(xtest, xtest, k, ftest);
+    double ndummy[gpudata::MAXREACTIONS];    
+    chem->computeForwardRateCoeffs(ndummy, xtest, xtest, k, ftest);
     double error = abs((fref - ftest[0]) / fref);
     if (error >= scalarErrorThreshold) {
       grvy_printf(GRVY_ERROR, "Rank %d - %.5E: %.5E\n", rank, xtest, abs((fref - ftest[0]) / fref));
