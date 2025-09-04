917bd7ecd3e35c6ff10beb3c50ad06f4243a2a10
Sigfried Haering <shaering@oden.utexas.edu>
2025-09-03 19:57:02 -0700

    c++ const issue, rolling back change to be compatible with automated tests

--- src/utils.cpp
+++ src/utils.cpp
@@ -1459,7 +1459,13 @@ void GradientVectorGridFunctionCoefficient::Eval(DenseMatrix &G, ElementTransfor
     // NB: In mfem/fem/coefficients.cpp, the function RefinedToCoarse is defined.  Here we reproduce it explicitly.
     //
     // ElementTransformation *coarse_T = RefinedToCoarse(*gf_mesh, T, ip, coarse_ip);
-    const Mesh &fine_mesh = *T.mesh;
+
+    // for mfem 4.8 & c++17
+    // const Mesh &fine_mesh = *T.mesh;
+
+    // older versions
+    Mesh &fine_mesh = *T.mesh;
+
     // Get the element transformation of the coarse element containing the
     // fine element.
     int fine_element = T.ElementNo;
