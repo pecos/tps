5d571a5bd0715b6b9424ffc182df79261e9778bc
Todd A. Oliver <oliver@oden.utexas.edu>
2025-09-04 16:29:51 -0500

    Minimal mods to make argon transport unit tests pass.
    1) Ensure new constant transport flag is set to false
    
    2) Set indices for neutral and ion in M2ulPhyS.  It appears that old
    ArgonMixtureTransport ctor did not use these and so test worked even
    though they were uninitialized, but new GasMixtureTransport ctor does
    use them.

--- test/test_argon_minimal.cpp
+++ test/test_argon_minimal.cpp
@@ -69,6 +69,9 @@ int main (int argc, char *argv[])
   double pressure = 101325.0;
 
   PerfectMixture *mixture = new PerfectMixture(srcConfig, dim, dim);
+
+  srcConfig.gasTransportInput.constantActive = false;
+
   TransportProperties *transport;
   switch (srcConfig.GetTranportModel()) {
     case ARGON_MINIMAL:
