f3c495e7250db75456842808c1545b7d20140281
ashwathsv <ashwathsv1996@gmail.com>
2026-06-17 12:44:25 -0500

    ensuring code matches with dev-spark branch.

--- src/M2ulPhyS.cpp
+++ src/M2ulPhyS.cpp
@@ -747,7 +747,7 @@ void M2ulPhyS::initVariables() {
                       mixture, d_mixture, chemistry_, transportPtr, radiation_, vfes, fes, gpu_precomputed_data_,
                       maxIntPoints, maxDofs, A, Aflux, mesh, spaceVaryViscMult, U, Up, gradUp, gradUpfes, gradUp_A,
                       bcIntegrator, config, plasma_conductivity_, joule_heating_, distance_);
-  
+
   CFL = config.GetCFLNumber();
   rhsOperator->SetTime(time);
   timeIntegrator->Init(*rhsOperator);
